460774b36e265f59a7482fe51b45f001b5b291b0
pomdpin <pomdpin@gmail.com>
2026-04-28 14:51:30 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,7 @@ if st.button("Générer / Generate"):
         st.error(f"Error loading data: {e}")
         st.stop()
 
-    prenoms = namemaker.make_name_set(names["Prénoms"], order=1, name_len_func=len, clean_up=True, 
-                                      exclude_real_names = True, exclude_history = True, add_to_history = True , n_candidates = 5)
+    prenoms = namemaker.make_name_set(names["Prénoms"], order=1, name_len_func=len, clean_up=True, exclude_real_names = True, exclude_history = True, add_to_history = True , n_candidates = 5)
     noms = namemaker.make_name_set(names["Noms de famille"], order=1, name_len_func=len, clean_up=True)
 
     pnjs = []
